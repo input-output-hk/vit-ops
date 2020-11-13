cc1f2968e3f91138fe031485afa61109bf32f2b4
John A. Lotoski <39752197+johnalotoski@users.noreply.github.com>
2020-11-13 17:30:38 -0500

    Switch to using musl vit-servicing-station, docker debug layers (#7)
    Co-authored-by: Samuel Leathers <samuel.leathers@iohk.io>

--- overlay.nix
+++ overlay.nix
@@ -3,8 +3,15 @@ let extraJobArgs = { };
 in final: prev:
 let lib = final.lib;
 in {
-  inherit (self.inputs.rust-libs.legacyPackages.${system})
-    vit-servicing-station;
+  vit-servicing-station = final.runCommand "vit-servicing-station-static" {
+    src = final.fetchurl {
+      url = "https://github.com/mzabaluev/vit-servicing-station/releases/download/v0.1.0-ci-test.1/vit-servicing-station-0.1.0-ci-test.1-x86_64-unknown-linux-musl.tar.gz";
+      sha256 = "sha256-esVtO4GzQob7Xev1RzaBq7SU1u4noCml2lAfghRJuHg=";
+    };
+  } ''
+    mkdir -pv $out/bin
+    tar -xvf $src -C $out/bin/
+  '';
 
   nomadJobs = let
     jobsDir = ./jobs;
