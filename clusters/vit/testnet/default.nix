27f7b15e8106cc02561a170b327945ec2909da2b
Michael Fellinger <michael.fellinger@iohk.io>
2020-12-09 20:53:24 +0100

    bump bitte

--- clusters/vit/testnet/default.nix
+++ clusters/vit/testnet/default.nix
@@ -82,8 +82,6 @@ in {
             "${self.inputs.nixpkgs}/nixos/modules/virtualisation/ec2-data.nix"
             "${extraConfig}"
             ./secrets.nix
-            # ./host_volumes.nix
-            # ./seaweedfs.nix
           ];
 
           securityGroupRules = {
