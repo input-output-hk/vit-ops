0bb34f39096d320210304d288747ecc2d467bb5f
John Lotoski <john.lotoski@iohk.io>
2020-12-24 14:38:10 -0500

    Sets loki log retention to 28 days

--- clusters/vit/testnet/monitoring-server.nix
+++ clusters/vit/testnet/monitoring-server.nix
@@ -3,4 +3,8 @@
     name = "provisioned-vit-ops";
     options.path = ../../../contrib/dashboards;
   }];
+  services.loki.configuration.table_manager = {
+    retention_deletes_enabled = true;
+    retention_period = "28d";
+  };
 }
