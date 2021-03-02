aafff037f546124b5b5c9282e4f871deb226f51e
Michael Fellinger <michael.fellinger@iohk.io>
2021-03-02 15:16:28 +0100

    use jormungandr master

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       url = "github:input-output-hk/jormungandr-nix";
       flake = false;
     };
-    jormungandr.url = "github:input-output-hk/jormungandr?ref=v0.10.0-alpha.3";
+    jormungandr.url = "github:input-output-hk/jormungandr";
     vit-servicing-station.url =
       "github:input-output-hk/vit-servicing-station/use-rust-nix";
     cardano-node.url =
