0734cf463617d4342a18520f741bdd55970e132c
Michael Fellinger <michael.fellinger@iohk.io>
2021-03-02 15:22:58 +0100

    adjust binary caches to use hydra as well

--- clusters/vit/testnet/default.nix
+++ clusters/vit/testnet/default.nix
@@ -37,9 +37,17 @@ in {
   };
 
   nix = {
-    binaryCaches = [ "https://vit-ops.cachix.org" ];
-    binaryCachePublicKeys =
-      [ "vit-ops.cachix.org-1:LY84nIKdW7g1cvhJ6LsupHmGtGcKAlUXo+l1KByoDho=" ];
+    binaryCaches = [
+      "https://vit-ops.cachix.org"
+      "https://hydra.iohk.io"
+      "https://hydra.mantis.ist"
+    ];
+
+    binaryCachePublicKeys = [
+      "vit-ops.cachix.org-1:LY84nIKdW7g1cvhJ6LsupHmGtGcKAlUXo+l1KByoDho="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "hydra.mantis.ist-1:4LTe7Q+5pm8+HawKxvmn2Hx0E3NbkYjtf1oWv+eAmTo="
+    ];
   };
 
   cluster = {
