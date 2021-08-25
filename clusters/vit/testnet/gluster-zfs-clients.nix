069ff4cb79b89da949017b040a6c9939674c24a7
Michael Fellinger <michael.fellinger@iohk.io>
2021-08-25 12:39:06 +0200

    more zfs tuning

--- clusters/vit/testnet/gluster-zfs-clients.nix
+++ clusters/vit/testnet/gluster-zfs-clients.nix
@@ -1,6 +1,34 @@
-{ ... }: {
+{ lib, ... }: {
   fileSystems."/mnt/gv1" = {
     device = "glusterd.service.consul:/gv1";
     fsType = "glusterfs";
+  };
+
+  services.zfs = {
+    autoSnapshot = {
+      enable = true;
+      monthly = lib.mkForce 0;
+      hourly = 0;
+      frequent = 0;
+      daily = 2;
+    };
+  };
+
+  # Change behaviour to only take snapshots of /var because everything else is irrelevant
+  systemd.services.zfs-snapshot-enable = lib.mkForce {
+    script = ''
+      set -euo pipefail
+      echo "The current state of zfs autosnapshots is:"
+      zfs get com.sun:auto-snapshot
+      echo " "
+      zfs set com.sun:auto-snapshot=false tank
+      zfs set com.sun:auto-snapshot=true tank/var
+      echo "The new state of zfs autosnapshots is:"
+      zfs get com.sun:auto-snapshot
+      echo " "
+      echo "The current size of existing zfs snapshots is:"
+      zfs list -o space -t filesystem,snapshot
+      echo " "
+    '';
   };
 }
