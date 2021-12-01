e65f7d265d09b74eb045bf438c656a3dc700ec6c
Caslav Nedeljkovic (const-iohk) <caslav.nedeljkovic@iohk.io>
2021-12-01 14:20:42 +0000

    Remove mantis hydra from binary caches

--- clusters/vit/testnet/default.nix
+++ clusters/vit/testnet/default.nix
@@ -47,11 +47,10 @@ in {
   };
 
   nix = {
-    binaryCaches = [ "https://hydra.iohk.io" "https://hydra.mantis.ist" ];
+    binaryCaches = [ "https://hydra.iohk.io" ];
 
     binaryCachePublicKeys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "hydra.mantis.ist-1:4LTe7Q+5pm8+HawKxvmn2Hx0E3NbkYjtf1oWv+eAmTo="
     ];
   };
 
