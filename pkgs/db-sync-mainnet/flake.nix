c36fd600659501da9fcc520cf372d8b5c7586cfe
Rok Garbas <rok@garbas.si>
2022-05-11 12:24:41 +0200

    updating db-sync snapshots

--- pkgs/db-sync-mainnet/flake.nix
+++ pkgs/db-sync-mainnet/flake.nix
@@ -13,8 +13,8 @@
     };
 
     restoreSnapshot =
-      "https://update-cardano-mainnet.iohk.io/cardano-db-sync/12/db-sync-snapshot-schema-12-block-7087276-x86_64.tgz";
+      "https://update-cardano-mainnet.iohk.io/cardano-db-sync/12/db-sync-snapshot-schema-12-block-7230633-x86_64.tgz";
     restoreSnapshotSha =
-      "73cfca460ed64762fab880f7bd63957e2c369d84992e58c0f1215c5b44e43ed2";
+      "5325407ae9cfdd63c6ae9e63f03d5f5b86c53be9f3ab03526cc23852ce6bb443";
   };
 }
