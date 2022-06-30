d3d8890ffa13b8246ec9b273f36d38149789ff65
Caslav Nedeljkovic <caslav.nedeljkovic@iohk.io>
2022-06-30 11:10:58 +0200

    Updating db-sync testnet to 13.0.0

--- pkgs/db-sync-testnet/flake.nix
+++ pkgs/db-sync-testnet/flake.nix
@@ -13,8 +13,8 @@
     };
 
     restoreSnapshot =
-      "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3590461-x86_64.tgz";
+      "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/13/db-sync-snapshot-schema-13-block-3269999-x86_64.tgz";
     restoreSnapshotSha =
-      "922bb683158f36380d9e54399a27ac3bb5d760bdd4bc55f00f35ea40ae6d0f5f";
+      "c020bb176b05f58ce1b15420f590c47c9e150efcba099a605215da46fc28322d";
   };
 }
